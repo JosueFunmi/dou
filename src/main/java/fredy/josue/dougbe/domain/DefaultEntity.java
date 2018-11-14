9946785ba95100706f9222121a9124409e4240c6
Fredy Josue <josue.wanou@gmail.com>
2018-11-14 12:05:34 +0100

    preparation des

---
+++ src/main/java/fredy/josue/dougbe/domain/DefaultEntity.java
@@ -0,0 +1,46 @@
+package fredy.josue.dougbe.domain;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Date;
+
+import javax.persistence.ColumnResult;
+import javax.persistence.ConstructorResult;
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SqlResultSetMapping;
+import javax.persistence.SqlResultSetMappings;
+
+import fredy.josue.dougbe.util.ResultInfo;
+
+
+
+
+@Entity
+@SqlResultSetMappings({
+
+@SqlResultSetMapping(name="ResultInfo",
+classes = {
+    @ConstructorResult(
+
+            targetClass = ResultInfo.class,
+            columns = {
+                @ColumnResult(name = "resultat",  type = String.class)
+            })
+}),
+
+})
+
+
+public class DefaultEntity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+}
